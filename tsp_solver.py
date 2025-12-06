7ae1ede789d59014e250c8fa39bcaccc7048b608
BahrieVeseli <bahrie.veseli@student.uni-pr.edu>
2025-12-06 23:46:43 +0100

    update tsp_solver.py

--- tsp_solver.py
+++ tsp_solver.py
@@ -1,7 +1,11 @@
 import sys, os, time, random, math
 from typing import List, Tuple, Dict, Optional
-
-INF = 10**15
+from cost_eval import (
+    INF,
+    tour_arcs_from_nodes,
+    compute_tour_cost_from_arcseq,
+    compute_tour_cost
+)
 
 def parse_instance(path: str):
     with open(path, "r", encoding="utf-8") as f:
@@ -48,30 +52,6 @@ def parse_instance(path: str):
                     continue
     return n_nodes, arcs, arc_by_uv, relations
 
-def tour_arcs_from_nodes(tour_nodes, arc_by_uv):
-    arc_seq = []
-    for i in range(len(tour_nodes)-1):
-        a = arc_by_uv.get((tour_nodes[i], tour_nodes[i+1]))
-        if a is None: return None
-        arc_seq.append(a)
-    return arc_seq
-
-def compute_tour_cost_from_arcseq(arc_seq, arcs, trigger_map_by_trigger):
-    last_activation = {}
-    total = 0.0
-    for pos, a in enumerate(arc_seq):
-        if a in last_activation:
-            total += last_activation[a]
-        else:
-            total += arcs[a][2]
-        for (targ, newc) in trigger_map_by_trigger.get(a, []):
-            last_activation[targ] = newc
-    return total
-
-def compute_tour_cost(tour_nodes, arcs, arc_by_uv, trigger_map_by_trigger):
-    arc_seq = tour_arcs_from_nodes(tour_nodes, arc_by_uv)
-    if arc_seq is None: return INF, False
-    return compute_tour_cost_from_arcseq(arc_seq, arcs, trigger_map_by_trigger), True
 
 
 def greedy_construct(n_nodes, arcs, arc_by_uv, trigger_map_by_trigger, attempts=200):
